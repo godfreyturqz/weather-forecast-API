7cf5839a540c381117674a30a9dbda55c2a49400
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-10-16 22:48:32 +0800

    css changes

--- src/components/ApiData/ApiData.js
+++ src/components/ApiData/ApiData.js
@@ -1,50 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import './ApiData.css'
+import './ApiData.css';
 
 const ApiData = () => {
     const [jsondata, setJsondata] = useState([])
 
-    //have used axios to get data from requested url
+    //have used axios to get data from api url
     useEffect( ()=> {
-        axios.get('https://api.climacell.co/v3/weather/forecast/daily?lat=15.210185&lon=120.581434&unit_system=si&start_time=now&fields=humidity%2Cprecipitation_probability%2Cweather_code&apikey=cvSzTMRvkYmD0e7ok4nervQCvI0MmAF9')
-            .then(response => {
-                setJsondata(response.data)
-            })
-            .catch(error => console.log(error))
+        axios
+        .get('https://api.climacell.co/v3/weather/forecast/daily?lat=15.210185&lon=120.581434&unit_system=si&start_time=now&fields=humidity%2Cprecipitation_probability%2Cweather_code%2Ctemp&apikey=cvSzTMRvkYmD0e7ok4nervQCvI0MmAF9')
+        .then(response => {
+            setJsondata(response.data)
+        })
+        .catch(error => console.log(error))
     })
-
-
+    
     return (
         <>
             <div className="container">
-                <h1>Daily Weather Forecast by climacellAPI</h1>
-                <div className="row rowHead">
-                    <p>Date</p>
-                    <p>Weather</p>
-                    <p>Precipitation Probability</p>
-                    <p>Humidity (min)</p>
-                    <p>Humidity (max)</p>
+                <div className="wrapper">
+                    <h1>Daily Weather Forecast</h1>
+                    <div className="row rowHead">
+                        <p>Date</p>
+                        <p>Weather</p>
+                        <p>Precipitation Probability</p>
+                        <p>Humidity (min.)</p>
+                        <p>Humidity (max.)</p>
+                        <p>Temp (min.)</p>
+                        <p>Temp (max.)</p>
+                    </div>
+                    {jsondata.map ( obj => {
+                        return (
+                            <div className="row">
+                                <p>{obj.observation_time.value}</p>
+                                <p>{obj.weather_code.value}</p>
+                                <p>{obj.precipitation_probability.value}</p>
+                                <p>{obj.humidity[0].min.value}</p>
+                                <p>{obj.humidity[1].max.value}</p>
+                                <p>{obj.temp[0].min.value}</p>
+                                <p>{obj.temp[1].max.value}</p>
+                            </div>
+                        )
+                    })}
                 </div>
-                <div className="row">
-                    <p>data...</p>
-                    <p>data...</p>
-                    <p>data...</p>
-                    <p>data...</p>
-                    <p>data...</p>
+                <div className="wrapper">
+                    <p>API by ClimaCell</p>
                 </div>
-                {jsondata.map ( jsondata => {
-                    return (
-                        <div className="row">
-                            <p>{jsondata.observation_time.value}</p>
-                            <p>{jsondata.weather_code.value}</p>
-                            <p>{jsondata.precipitation_probability.value}</p>
-                            <p>{jsondata.humidity[0].min.value}</p>
-                            <p>{jsondata.humidity[1].max.value}</p>
-                        </div>
-                    )
-                })}
             </div>
+            
         </>
     )
 }
